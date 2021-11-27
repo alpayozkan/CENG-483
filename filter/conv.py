3150647d3593eb61c7b2b79e5a358c2d90784a05
Alpay Özkan <alpayozkan@Alpays-MacBook-Pro.local>
2021-11-27 16:01:22 +0300

    filters

--- filter/conv.py
+++ filter/conv.py
@@ -42,6 +42,18 @@ shift_right = np.array([[0, 0, 0],
                       [1, 0, 0], 
                       [0, 0, 0]])
 
+shift_left = np.array([[0, 0, 0], 
+                      [0, 0, 1], 
+                      [0, 0, 0]])
+
+gauss = (1/16)*np.array([[1, 2, 1], 
+                        [2, 4, 2], 
+                        [1, 2, 1]], dtype=np.float64)
+
+inverse_gauss = (1/25)*np.array([[4, 2, 4], 
+                                [2, 1, 2], 
+                                [4, 2, 4]], dtype=np.float64)
+
 # animal.png de hata veriyor
 # libpng warning: iCCP: known incorrect sRGB profile
 
@@ -51,9 +63,13 @@ def applyKernel(img, kernel, n):
     return img
 
 image = cv2.imread('images/flower.png')
-result = applyKernel(image, shift_right, 40)
+result = applyKernel(image, inverse_gauss, 10)
+result2 = applyKernel(image, gauss, 10)
 
-cv2.imshow('frame1', result)
-cv2.imshow('frame2', image)
+
+cv2.imshow('image', image)
+cv2.imshow('result-1', result)
+cv2.imshow('result-2', result2)
+
 cv2.waitKey(0)
 
